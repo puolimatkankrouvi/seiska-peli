b9f83b9846ab5cb2a00862cb6bb4bfee5dfcc61d
puolimatkankrouvi <cde_1414@yahoo.com>
2017-05-05 17:53:59 +0300

    Update grafiikka.cpp

--- grafiikka.cpp
+++ grafiikka.cpp
@@ -16,28 +16,28 @@ using namespace std;
   
 void tulostaPoyta(Kortti poyta[13][4]){
   cout << endl;
-  for (int i = 12; i >= 0; --i)
-  {
+  for (int i = 12; i >= 0; --i){
     cout << "|";
-    for (int j = 0; j < 4; ++j)
-		{
-			if (poyta[i][j].arvo == EI_KORTTIA)
-			{
-				cout << "         |";
-			}
-			else{
-				string maa = maaksi(poyta[i][j].maa);
-				string arvo = arvoksi(poyta[i][j].arvo);
-         		cout << maa << arvo;
-         		int valimaara = 0;
-         		while(valimaara< (8-(maa.size())-(arvo.size()))){
-                  cout << " ";
-				  valimaara++;
-         		}
-         		cout << "|";
-			}
-    }
-	cout << endl;
+    for (int j = 0; j < 4; ++j){
+	  if (poyta[i][j].arvo == EI_KORTTIA){
+		//Jos pöydän kohdassa ei ole korttia
+		cout << "         |";
+	  }
+	  else{
+	    string maa = maaksi(poyta[i][j].maa);
+	    string arvo = arvoksi(poyta[i][j].arvo);
+        cout << maa << arvo;
+        int valimaara = 0;
+		//Sarakkeeseen tulostuu yhteensä 8 merkkiä,
+		//kortin arvo ja maa, loput välilyöntejä
+        while(valimaara< (8-(maa.size())-(arvo.size()))){
+          cout << " ";
+	      valimaara++;
+        }
+      cout << "|";
+	}
+  }
+  cout << endl;
   }
 }
 
@@ -49,18 +49,22 @@ string lueKomento(){
 
 void tulostaSijoitus(int sijoitus,int pelaaja, int n){
 
+	
 	if(sijoitus == 1){
+	  //Tulostetaan voittajan tiedot
 	  switch(pelaaja){
+		//Jos pelaaja oli ensimmäinen
         case 0:
 	    cout << "Voitit pelin!" << endl;
         break;
 	    case 1:
 	    switch(n){
 		  case 2:
-		  cout << "Tietokonepelaaja voitti." << endl;
+		    //Jos pelaajien lukumäärä n on vain kaksi, tietokonepelaajan numeroa ei tulosteta
+		    cout << "Tietokonepelaaja voitti." << endl;
 		  break;
 		  default:
-		  cout << "Tietokonepelaaja " << pelaaja << " voitti." << endl;
+		    cout << "Tietokonepelaaja " << pelaaja << " voitti." << endl;
 		  break;
 	    }
 	    break;
@@ -70,17 +74,18 @@ void tulostaSijoitus(int sijoitus,int pelaaja, int n){
 	}
 	else{
 	  switch(pelaaja){
+		//Muiden kuin voittajien sijoitukset
         case 0:
-	    cout << "Olit " << sijoitus << "." << endl;
+	      cout << "Olit " << sijoitus << "." << endl;
         break;
 	    default:
-		cout << "Tietokonepelaaja " << pelaaja << " " << "oli " << sijoitus << "."   << endl;
+		  cout << "Tietokonepelaaja " << pelaaja << " " << "oli " << sijoitus << "."   << endl;
 		break;
 	  }
 	}
 }
 
-void tulostaLaitto(int vuorossa,int arvo,int maa){
+void tulostaLaitto(int vuorossa, int arvo, int maa){
 	
 	if(maa == EI_KORTTIA){
 	  cout << "Tietokonepelaaja " << vuorossa << " sanoi ohi" << endl;
@@ -90,12 +95,13 @@ void tulostaLaitto(int vuorossa,int arvo,int maa){
 	}
 }
 
-void tulostaOtto(int vuorossa,int arvo,int maa){
+void tulostaOtto(int vuorossa, int arvo, int maa){
     if(vuorossa == IHMISPELAAJA){
 	  cout << "Otit edelliselta kortin " << maaksi(maa) << arvoksi(arvo) << endl;
 	}
 	else
 	  if(vuorossa == 1){
+		  //Ensimmäinen tietokonepelaaja ottaa aina ihmispelaajalta kortin
 		  cout << "Tietokonepelaaja " << vuorossa << " otti sinulta kortin " << maaksi(maa) << arvoksi(arvo) << endl;
 	  }
 	else{
